dfdd6aa5dfb5f81244c77ea91d50a11a748789ce
xiaonanchong <xnchong@outlook.com>
2019-12-04 10:51:12 +0800

    use vibration dataset

--- use_discriminator.py
+++ use_discriminator.py
@@ -5,7 +5,7 @@ new_model = tf.keras.models.load_model('trained_discriminator.h5')
 
 def load_data():
     slice_len = 500
-    data = loadmat('/home/anjie/data/270.mat')['X270_DE_time']
+    data = loadmat('/home/anjie/data/97.mat')['X097_DE_time']
     l = int(data.shape[0]/slice_len)*slice_len
     data_x = np.array(data[:l]).reshape(-1, slice_len) 
     data_y = np.array(['nor' for i in range(data_x.shape[0])])
@@ -22,6 +22,6 @@ for k in key:
     v = r_new.size
     result[k] = v
 print(result)
-print(float(result[False])/(result[False]+result[True]))
+print(1-float(result[False])/(result[False]+result[True]))
 
 
